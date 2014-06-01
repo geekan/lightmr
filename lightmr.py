adc4c7ac9045764d75a1ef68cf2af01576a9be03
geekan <geekan@foxmail.com>
2014-06-01 20:08:30 +0800

    update lightmr, write a simple async program

--- lightmr.py
+++ lightmr.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 #coding: utf8
 
+from multiprocessing import Pool
+
+def f(x):
+    return x**2
+
+def callback(x):
+    print(x)
+
 def main():
-    pass
+    pool = Pool(processes=1)
+    result = pool.apply_async(f, [10], callback)
 
 if __func__ == '__main__':
     main()
