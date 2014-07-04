2b4665527f3c3112d1284d618e302261818365d8
geekan <geekan@foxmail.com>
2014-07-04 11:13:07 +0800

    local mul process example

--- lightmr.py
+++ lightmr.py
@@ -2,16 +2,24 @@
 #coding: utf8
 
 from multiprocessing import Pool
+import time
 
 def f(x):
     return x**2
 
-def callback(x):
-    print(x)
+result_list = []
+def callback(result):
+    result_list.append(result)
+    # with open("test.log") as test_file:
+    #     test_file.write(x)
 
 def main():
     pool = Pool(processes=1)
-    result = pool.apply_async(f, [10], callback)
+    for i in range(10):
+        result = pool.apply_async(f, args=(i, ), callback=callback)
+    pool.close()
+    pool.join()
+    print(result_list)
 
 if __name__ == '__main__':
     main()
