4955f3f3dbe896251ea050add0c683ef8b061535
geekan <geekan@foxmail.com>
2014-08-01 01:54:05 +0800

    update..

--- lightmr.py
+++ lightmr.py
@@ -7,6 +7,37 @@
 # 3. Client set the plan and distribute it to all servers.
 
 
+import asyncore
+import socket
+
+
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(8192)
+        if data:
+            self.send(data)
+
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = EchoHandler(sock)
+
+server = EchoServer('localhost', 8080)
+asyncore.loop()
+
+
 # Role: controller, client, server
 #   Controller may be an optional role, because if we know all the ip,
 #   then it's not so important..
